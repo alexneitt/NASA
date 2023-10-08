a7561c89eb3dabec7e8a11110bc3895e99c39fd0
alexneitt <alexisrg620@gmail.com>
2023-10-07 19:19:07 -0600

    A

--- Stellar Voyage/main.js
+++ Stellar Voyage/main.js
@@ -19,7 +19,7 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const geometry = new THREE.SphereGeometry(24, 24, 24);
 const material =  new THREE.MeshStandardMaterial({ color: 0xFF6347 });
 const torus= new THREE.Mesh(geometry, material);
 scene.add(torus);
